dc3528990bc94093d01513dbfdc6c0c3cb9cdefc
YamanUCSC <43651596+YamanUCSC@users.noreply.github.com>
2018-10-09 11:25:24 -0700

    Add files via upload

---
+++ numpy.py
@@ -0,0 +1,11 @@
+import numpy as np 
+
+x = 1.0 	#define a float 
+y = 2.0 	#define another float 
+
+#trigonometry 
+print(np.sin(x))	#sin(x)
+print(np.cos(x))	#cos(x)
+print(np.tan(x))	#tan(x)
+print(np.arcsin(x))	#arcsin(x)
+print(np.arccos(x))	#arctan(x)
